c1c26f0e2cd0150baeb08bcb2f89a4d57e8b8740
Bailey Frye <baileyfrye@Baileys-Mac-mini.local>
2024-03-25 16:44:39 -0500

    Initial commit

---
+++ src/books.js
@@ -0,0 +1,25 @@
+import img1 from './images/book1.jpg';
+import img2 from './images/book2.jpg';
+import img3 from './images/book3.jpg';
+
+export const books = [
+  {
+    title:
+      'Dog Man: The Scarlet Shedder: A Graphic Novel (Dog Man #12): From the Creator of Captain Underpants',
+    author: 'Dav Pilkey',
+    img: img1,
+    id: 1,
+  },
+  {
+    title: 'How to Catch the Easter Bunny',
+    author: 'Adam Wallace',
+    img: img2,
+    id: 2,
+  },
+  {
+    title: "It's Not Easy Being a Bunny: An Easter Book for Kids",
+    author: 'Marilyn Sadler',
+    img: img3,
+    id: 3,
+  },
+];
